b76e20c8366622169b55a35b2d245cd4a49aff6c
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-03-17 11:55:13 -0700

    Basic email table

---
+++ src/components/MailTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <table>
+    <thead>
+
+    </thead>
+    <tbody>
+      <tr v-for="email in emails" :key="email.id">
+        <td><input type="checkbox" /></td>
+        <td>{{email.subject}}</td>
+        <td>{{email.body}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    props: {
+      emails: {
+        type: Array,
+        default: []
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  table {
+    max-width: 800px;
+    margin: auto;
+  }
+  td {
+    text-align: left;
+  }
+</style>
