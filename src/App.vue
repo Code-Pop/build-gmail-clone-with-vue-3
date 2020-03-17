913092a419191062a14144038b67cdeda818a355
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-03-17 11:21:21 -0700

    InboxScreen start (replace HelloWorld)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InboxScreen />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import InboxScreen from './components/InboxScreen.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    InboxScreen,
   },
 };
 </script>
---
+++ src/components/InboxScreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>VMail Inbox</h1>
+
+  <div>Table will go here</div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
