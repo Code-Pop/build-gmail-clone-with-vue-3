11c6ac7badd4ad564e6b0c1361e7629010d522e1
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-03-29 01:38:36 -0700

    Bare app - remove everything except styling, db.json, and packages

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,13 @@
 <template>
   <div id="app">
-    <button @click="selectScreen('MailScreenInbox');"
-            :disabled="screenName == 'MailScreenInbox'">
-      Inbox View
-    </button>
-    <button @click="selectScreen('MailScreenArchived')" 
-            :disabled="screenName == 'MailScreenArchived'">
-      Archived View
-    </button>
 
-    <suspense>
-      <template #default>
-        <MailScreen :screenName="screenName" />
-      </template>
-      <template #fallback>
-        <p>Loading...</p>
-      </template>
-    </suspense>
   </div>
 </template>
 
 <script>
-import MailScreen from '@/components/MailScreen.vue';
-import useEmailSelection from './composition/useEmailSelection';
-import { ref } from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    MailScreen
-  },
-  setup(){  
-    let screenName = ref('MailScreenInbox');
-    let {emailSelection} = useEmailSelection();
-    let selectScreen = function(newScreen){
-      screenName.value = newScreen;
-      emailSelection.clear();
-    }
-    return {screenName, selectScreen}
-  }
+  name: 'App'
 };
 </script>
 
