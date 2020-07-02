fa0cb214f8b19b747863b851f4f518cf978d8b4b
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-07-01 18:03:46 -0700

    Video 7 - Close modal when hitting escape key

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -8,8 +8,23 @@
 </template>
 
 <script>
+  import { onBeforeUnmount } from 'vue'
+  
   export default {
     setup(props, {emit}) {
+      let onKeydown = (event) => {
+        console.log(event.key)
+        
+        if(event.key == 'Escape') {
+          emit('closeModal')
+        }
+      }
+
+      window.addEventListener('keydown', onKeydown)
+      onBeforeUnmount(()=>{
+        window.removeEventListener('keydown', onKeydown)
+      })
+
       return {
         emit
       }
